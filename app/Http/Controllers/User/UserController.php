3e632f8e7c844d4c2cb680050393683721e56d83
emon273273 <emon.cse273@gmail.com>
2025-09-09 22:26:53 +0600

    Implement user authentication with Sanctum and set up user routes

---
+++ app/Http/Controllers/User/UserController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers\User;
+
+use App\Http\Controllers\Controller;
+use Firebase\JWT\JWT;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    //
+
+    public function index()
+    {
+
+    
+    }
+}
